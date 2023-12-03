9c8f6c1b7c131ba3112f313c0849a1b840db0dca
Uri K <37979288+katzurik@users.noreply.github.com>
2023-12-03 11:04:03 +0200

    Create download_and_unzip.sh

---
+++ supervised_NER/download_and_unzip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define URLs
+TRAIN_SET_URL="https://storage.googleapis.com/neretrieve_dataset/supervised_ner/NERetrive_sup_train.jsonl.bz2"
+TEST_SET_URL="https://storage.googleapis.com/neretrieve_dataset/supervised_ner/NERetrive_sup_test.jsonl.bz2" 
+
+# Download files
+echo "Downloading Train Set..."
+wget "$TRAIN_SET_URL" -O "NERetrive_IR_train.jsonl.bz2"
+
+echo "Downloading Test Set..."
+wget "$TEST_SET_URL" -O "NERetrive_IR_test.jsonl.bz2" 
+
+# Unzip files
+
+echo "Unzipping Train Set..."
+bzip2 -d "NERetrive_sup_train.jsonl.bz2"
+
+echo "Unzipping Test Set..."
+bzip2 -d "NERetrive_sup_test.jsonl.bz2" 
+
+echo "Download and extraction complete."
